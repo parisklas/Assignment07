30854a05e777955c089281d56bec92220bebcf62
Paris Klas <138509092+parisklas@users.noreply.github.com>
2023-11-15 22:22:01 -0400

    assignment

--- js/script-array-practice.js
+++ js/script-array-practice.js
@@ -1,33 +1,212 @@
 //STEP 1
+  // let favMovies = ['Inception', 'Get Out', 'Bridesmaids', 'Donnie Darko', 'A Beautiful Mind'];
+  // console.log(favMovies[1]);
 
 //STEP 2
+  // let movies = new Array(5);
+  // movies[0] = 'Inception';
+  // movies[1] = 'Get Out';
+  // movies[2] = 'Bridesmaids';
+  // movies[3] = 'Donnie Darko';
+  // movies[4] = 'A Beautiful Mind';
+  // console.log(movies[0]);
 
 //STEP 3
+  // let movies = new Array(5);
+  // movies[0] = 'Inception';
+  // movies[1] = 'Get Out';
+  // movies[2] = 'Bridesmaids';
+  // movies[3] = 'Donnie Darko';
+  // movies[4] = 'A Beautiful Mind';
+  // movies.splice(2, 0,'Scary Movie');
+  // console.log(movies.length);
 
 //STEP 4
+  // let movies = [];
+  // movies[0] = 'Inception';
+  // movies[1] = 'Get Out';
+  // movies[2] = 'Bridesmaids';
+  // movies[3] = 'Donnie Darko';
+  // movies[4] = 'A Beautiful Mind';
+  // delete movies[0];
+  // console.log(movies);
 
 //STEP 5
+  // let movies = [];
+  // movies[0] = 'Inception';
+  // movies[1] = 'Get Out';
+  // movies[2] = 'Bridesmaids';
+  // movies[3] = 'Donnie Darko';
+  // movies[4] = 'A Beautiful Mind';
+  // movies[5] = 'Scary Movie';
+  // movies[6] = 'No One is Going to Save You';
+  // for (let i = 0; i < movies.length; i++) {
+  //   console.log(movies[i]);
+  // }
 
 //STEP 6
+  // let movies = [];
+  // movies[0] = 'Inception';
+  // movies[1] = 'Get Out';
+  // movies[2] = 'Bridesmaids';
+  // movies[3] = 'Donnie Darko';
+  // movies[4] = 'A Beautiful Mind';
+  // movies[5] = 'Scary Movie';
+  // movies[6] = 'No One is Going to Save You';
+  // for (let i of movies) {
+  //   console.log(i);
+  // }
 
 //STEP 7
-
+  // let movies = [];
+  // movies[0] = 'Inception';
+  // movies[1] = 'Get Out';
+  // movies[2] = 'Bridesmaids';
+  // movies[3] = 'Donnie Darko';
+  // movies[4] = 'A Beautiful Mind';
+  // movies[5] = 'Scary Movie';
+  // movies[6] = 'No One is Going to Save You';
+  // for (let i of movies) {
+  //     let sorted = movies.sort();
+  //     console.log(sorted);
+  // }
 //STEP 8
+  // let movies = [];
+  // movies[0] = 'Inception';
+  // movies[1] = 'Get Out';
+  // movies[2] = 'Bridesmaids';
+  // movies[3] = 'Donnie Darko';
+  // movies[4] = 'A Beautiful Mind';
+  // movies[5] = 'Scary Movie';
+  // movies[6] = 'No One is Going to Save You';
 
+  // let leastFavMovies = ['The Little Mermaid', 'Inherent Vice', 'Twilight'];
+  // console.log('Movies I like:');
+  // console.log('');
+  // for (let i of movies) {
+  //   console.log(i);
+  // }
+  // console.log('');
+  // console.log('Movies I regret watching:');
+  // console.log('');
+  // for (let n of leastFavMovies) {
+  //   console.log(n);
+  // }
 //STEP 9
+  // let movies = [];
+  // movies[0] = 'Inception';
+  // movies[1] = 'Get Out';
+  // movies[2] = 'Bridesmaids';
+  // movies[3] = 'Donnie Darko';
+  // movies[4] = 'A Beautiful Mind';
+  // movies[5] = 'Scary Movie';
+  // movies[6] = 'No One is Going to Save You';
+
+  // let leastFavMovies = ['The Little Mermaid', 'Inherent Vice', 'Twilight'];
+
+  // let moviesNew = movies.concat(leastFavMovies);
+  // console.log(moviesNew.sort().reverse());
 
 //STEP 10
+  // let movies = [];
+  // movies[0] = 'Inception';
+  // movies[1] = 'Get Out';
+  // movies[2] = 'Bridesmaids';
+  // movies[3] = 'Donnie Darko';
+  // movies[4] = 'A Beautiful Mind';
+  // movies[5] = 'Scary Movie';
+  // movies[6] = 'No One is Going to Save You';
+
+  // let leastFavMovies = ['The Little Mermaid', 'Inherent Vice', 'Twilight'];
+
+  // let moviesNew = movies.concat(leastFavMovies);
+  // console.log(moviesNew.sort().reverse());
+  // console.log(moviesNew.pop());
 
 //STEP 11
+  // let movies = [];
+  // movies[0] = 'Inception';
+  // movies[1] = 'Get Out';
+  // movies[2] = 'Bridesmaids';
+  // movies[3] = 'Donnie Darko';
+  // movies[4] = 'A Beautiful Mind';
+  // movies[5] = 'Scary Movie';
+  // movies[6] = 'No One is Going to Save You';
+
+  // let leastFavMovies = ['The Little Mermaid', 'Inherent Vice', 'Twilight'];
+
+  // let moviesNew = movies.concat(leastFavMovies);
+  // console.log(moviesNew.sort().reverse());
+  // console.log(moviesNew.shift());
 
 //STEP 12
+  // let movies = [];
+  // movies[0] = 'Inception';
+  // movies[1] = 'Get Out';
+  // movies[2] = 'Bridesmaids';
+  // movies[3] = 'Donnie Darko';
+  // movies[4] = 'A Beautiful Mind';
+  // movies[5] = 'Scary Movie';
+  // movies[6] = 'No One is Going to Save You';
+
+  // let leastFavMovies = ['The Little Mermaid', 'Inherent Vice', 'Twilight'];
+  // let moviesNew = movies.concat(leastFavMovies);
+  // console.log(moviesNew);
+  // console.log(moviesNew.sort().reverse());
+  // console.log(moviesNew.indexOf('The Little Mermaid'));
+  // console.log(moviesNew.indexOf('Inherent Vice'));
+  // console.log(moviesNew.indexOf('Twilight'));
+
+  // moviesNew[1] = 'Parasite';
+  // moviesNew[4] = 'THe Dark Knight';
+  // moviesNew[0] = 'Seven';
+
+  // console.log(moviesNew);
+
 
 //STEP 13
+  // let movies = [["Get Out", 1], ["Inception", 2], ["No One is Going to Save You", 3], ["Bridesmaids", 4], ["Scary Movie", 5]];
+
+  // movies.forEach(function (movies) {
+  //    let movieNames = movies.filter(function(names){
+  //    return typeof names !== "number";});
+  //    console.log(movieNames);
+  // })
 
 //STEP 14
+  // let employees = ['Zak', 'Jessica', 'Mark', 'Fred', 'Sally'];
+
+  // let showEmployee = function() {
+  //     console.log('Employees:');
+  //     console.log('');
+  //     employees.forEach((employee) => {
+  //       console.log(employee.toUpperCase());
+  //     })
+  // }
+  // showEmployee(employees);
 
 //STEP 15
+  // function filter_array_values(arr) {
+  //   arr = arr.filter(isEligible);
+  //   return arr;
+  // }
+  // function isEligible(value) {
+  //   if(value !== false || value !== null || value !== 0 || value !== "") {
+  //     return value;
+  //   }
+  // }
+  // console.log(filter_array_values([58, '', 'abcd', true, null, false, 0]));
 
 //STEP 16
 
+  // function random_item(items)
+  // {
+  // return items[Math.floor(Math.random()*items.length)];
+  // }
+
+  // let items = [2, 45, 63, 149, 77, 76, 100, 8, 82, 272];
+  // console.log(random_item(items));
+
 //STEP 17
+  // let arr = [2, 45, 63, 500, 77, 76, 100, 8, 82, 272];
+  // console.log(Math.max.apply(null, arr));
--- js/script-ems.js
+++ js/script-ems.js
@@ -1,59 +1,86 @@
 // CREATE AN ARRAY OF EMPLOYEES
-
+let employees = [
+    [12762737, 'Frank Ocean', 4474, 'frank@ocean.com', 'Executive'],
+    [83726394, 'Blake Lively', 8383, 'blake@lively.com', 'Marketing'],
+    [28294629, 'Tom Hanks', 7830, 'tom@hanks.com', 'Sales'],
+    [91837465, 'Gerard Butler', 9274, 'gerard@butler.com', 'Engineering'],
+    [82828490, 'Frida Kahlo', 8286, 'frida@kahlo.com', 'Quality Assurance']
+];
 
 // CHECK TO SEE IF STORAGE OBJECT EXISTS WHEN THE PAGE LOADS
 // IF DOES, RETURN STORAGE OBJECT INTO ARRAY INSTEAD OF POPULATED ARRAY
-
+if (localStorage.getItem('employees')) {
+    employees = JSON.parse(localStorage.getItem('employees'))
+}
 
 // GET DOM ELEMENTS
-
+let form = document.getElementById('addForm');
+let empTable = document.getElementById('empTable');
+let count = document.getElementById('empCount');
+// let submitBtn = document.getElementById('submit');
 
 // BUILD THE EMPLOYEES TABLE WHEN THE PAGE LOADS
-
+buildGrid();
 
 // ADD EMPLOYEE
 form.addEventListener('submit', (e) => {
     // PREVENT FORM SUBMISSION
-
+    e.preventDefault();
     // GET THE VALUES FROM THE TEXT BOXES
-
+    let id = document.getElementById('id').value;
+    let name = document.getElementById('name').value;
+    let ext = document.getElementById('extension').value;
+    let email = document.getElementById('email').value;
+    let department = document.getElementById('department').value;
+    // let employees = JSON.parse(localStorage.getItem('employees'));
     // ADD THE NEW EMPLOYEE TO A NEW ARRAY OBJECT
-
+    let newEmployee = [id, name, ext, email, department];
     // PUSH THE NEW ARRAY TO THE *EXISTING* EMPLOYEES ARRAY
+    employees.push(newEmployee);
 
     // BUILD THE GRID
+    buildGrid();
 
     // RESET THE FORM
-
+    form.reset();
     // SET FOCUS BACK TO THE ID TEXT BOX
-
+    document.getElementById('id').focus();
 });
 
 // DELETE EMPLOYEE
 empTable.addEventListener('click', (e) => {
     // CONFIRM THE DELETE
-
+    if (e.target.classList.contains('delete')) {
+        if (confirm('Are you sure you want to delete this employee?')) {
         // GET THE SELECTED ROWINDEX FOR THE TR (PARENTNODE.PARENTNODE)
-
+        let rowIndex = e.target.parentNode.parentNode.rowIndex;
         // REMOVE EMPLOYEE FROM ARRAY
-
+        employees.splice(rowIndex -1, 1);
         // BUILD THE GRID
-
+        buildGrid();
+        }
+    }
 });
 
 // BUILD THE EMPLOYEES GRID
 function buildGrid() {
     // REMOVE THE EXISTING SET OF ROWS BY REMOVING THE ENTIRE TBODY SECTION
-
+    empTable.lastElementChild.remove();
     // REBUILD THE TBODY FROM SCRATCH
-
+    let tbody = document.createElement('tbody');
     // LOOP THROUGH THE ARRAY OF EMPLOYEES
     // REBUILDING THE ROW STRUCTURE
-
+    employees.forEach((employee, index) => {
+        let row = tbody.insertRow();
+        Object.values(employee).forEach(text => {
+            let cell = row.insertCell();
+            cell.appendChild(document.createTextNode(text));
+        });
+    })
     // BIND THE TBODY TO THE EMPLOYEE TABLE
-
+    empTable.appendChild(tbody);
     // UPDATE EMPLOYEE COUNT
-
+    document.getElementById('empCount').innerText = employees.length;
     // STORE THE ARRAY IN STORAGE
-
+    localStorage.setItem('employees', JSON.stringify(employees));
 };
